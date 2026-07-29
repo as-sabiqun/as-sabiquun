b6031e81bf794e9c5790183b62d15ff376913641
as-sabiqun <as-sabiqun@users.noreply.github.com>
2026-07-29 11:09:27 +0800

    Remove Korban breadcrumb behind header

--- src/app/(marketing)/korban/page.tsx
+++ src/app/(marketing)/korban/page.tsx
@@ -14,13 +14,7 @@ export default async function KorbanPage() {
   return (
     <section className="py-10 lg:py-14">
       <div className="container">
-        <nav className="breadcrumb">
-          <Link href="/services">Services</Link>
-          <span aria-hidden="true">/</span>
-          <span>Korban</span>
-        </nav>
-
-        <div className="mt-6">
+        <div>
           {packages.length ? (
             <KorbanContent initialRequestId={randomUUID()} packages={packages} />
           ) : (
